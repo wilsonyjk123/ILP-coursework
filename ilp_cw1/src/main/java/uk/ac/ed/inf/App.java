ca5aca98b7b2a225264bfff070bbc26b0a2ea675
Win102020NJPLUC\Administrator <s1941321@ed.ac.uk>
2021-09-30 01:57:07 +0100

    Upload New Files

--- ilp_cw1/src/main/java/uk/ac/ed/inf/App.java
+++ ilp_cw1/src/main/java/uk/ac/ed/inf/App.java
@@ -1,5 +1,13 @@
 package uk.ac.ed.inf;
 
+import com.google.gson.Gson;
+
+import java.lang.reflect.Type;
+import java.net.MulticastSocket;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -9,5 +17,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Menus menus = new Menus("localhost","80");
+        menus.getResponse();
+        System.out.println(menus.jsonText);
+
     }
 }
--- ilp_cw1/src/main/java/uk/ac/ed/inf/Menus.java
+++ ilp_cw1/src/main/java/uk/ac/ed/inf/Menus.java
@@ -1,14 +1,21 @@
 package uk.ac.ed.inf;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Menus {
     String machineFirstName = "";
     String port = "";
+    String jsonText = "";
     private static final HttpClient client = HttpClient.newHttpClient();
 
 
@@ -19,16 +26,50 @@ public class Menus {
     }
 
     /*Returns the int cost in pence of having all of these items delivered by drone*/
-     public int getDeliveryCost(String... items) throws IOException, InterruptedException {
+     public int getDeliveryCost(String... items){
+         getResponse();
+         MenusInfo menusInfo = new Gson().fromJson(this.jsonText, (Type) MenusInfo.class);
+         List<item> list = new ArrayList<item>();
+         return 1;
+     }
+     public String setUrlString(){
          /*Set the urlString*/
-         String urlString = "http://"+machineFirstName+":"+port+"/menus/menus.json";
-
+         String urlString = "http://" + machineFirstName + ":" + port+"/menus/menus.json";
+         return urlString;
+     }
+     public HttpRequest buildRequest(){
          /*Create a request*/
+         String urlString = setUrlString();
          HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urlString)).build();
-
-         /*Create a response to receive data*/
-         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-         return items.length * 50;
+         return request;
      }
 
+     public void getResponse(){
+         HttpRequest request = buildRequest();
+         try{
+             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+             if(response.statusCode() == 200){
+                 this.jsonText = response.body();
+             }else{
+                 System.out.println("Connection Failed with Response Code: "+response.statusCode());
+             }
+         }catch(IOException e){
+             e.printStackTrace();
+             System.out.println("Connection Failed: IOException");
+         }catch(InterruptedException e){
+             e.printStackTrace();
+             System.out.println("Connection Failed: InterruptedException");
+         }
+     }
+
+
+     public class MenusInfo{
+         String name;
+         String location;
+         List<item> menu;
+     }
+     public class item{
+        String item;
+        int pence;
+     }
 }
