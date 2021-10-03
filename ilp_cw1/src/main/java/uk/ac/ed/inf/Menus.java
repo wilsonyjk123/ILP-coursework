d55bbb022bc9e2e709c7ad18108bce580d2ba5ad
Win102020NJPLUC\Administrator <s1941321@ed.ac.uk>
2021-10-03 22:01:37 +0100

    Upload New Files

--- ilp_cw1/src/main/java/uk/ac/ed/inf/Menus.java
+++ ilp_cw1/src/main/java/uk/ac/ed/inf/Menus.java
@@ -16,6 +16,8 @@ public class Menus {
     private String machineFirstName = "";
     private String port = "";
     private String jsonText = "";
+
+    // Initiate a HttpClient when an object is created
     private static final HttpClient client = HttpClient.newHttpClient();
 
     /**
@@ -61,23 +63,34 @@ public class Menus {
          // Initiate a counter to calculate the total cost
          int totalCost = 0;
          try{
+             // Get the response body into jsonText field
              getResponse();
-             ArrayList<MenusInfo> menusList;
+
+             // Parsing the text in json form and create an object of the corresponding inner class to access the attributes
              Type listType = new TypeToken<ArrayList<MenusInfo>>(){}.getType();
-             menusList = new Gson().fromJson(this.jsonText,listType);
-             for (MenusInfo m: menusList){
-                 for(MenusInfo.Item i: m.menu){
+             ArrayList<MenusInfo> menusList = new Gson().fromJson(this.jsonText,listType);
+
+             // Iterate each MenusInfo object in the menuList
+             for (MenusInfo mi: menusList){
+
+                 // Iterate each item object in the menu
+                 for(MenusInfo.Item i: mi.menu){
+
+                     // Iterate each string in the given parameter - strings
                      for(String s:strings){
+
+                         // Compare each of these two strings
                          if(i.item.equals(s)){
+                             // Add the money to the counter if the two strings correspond
                              totalCost += i.pence;
                          }
 
                      }
                  }
              }
-         }catch (IllegalArgumentException|NullPointerException e){
+         }catch (IllegalArgumentException | NullPointerException e){
              e.printStackTrace();
-             System.exit(1);
+             System.exit(1); // Unsuccessful termination
          }
         return totalCost + 50; // 50 pence for extra delivery fee
      }
@@ -85,22 +98,29 @@ public class Menus {
     /**
      * Connect to the target server and get the response of the request
      *
-     * @throws IOException or InterruptedException If connecting to server is failed or A thread interruption operation has occurred
+     * @throws IOException or InterruptedException If a send request is failed
      */
      public void getResponse(){
+         // Set the urlString by using the getter
          String urlString = getServerAddressToMenus();
+
+         // Create a GET request with the urlString
          HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urlString)).build();
+
          try{
+             // Send the request to the target server
              HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+
+             // If the status code of the response is 200, then we can use response.body()
              if(response.statusCode() == 200){
                  this.jsonText = response.body();
              }else{
-                 System.out.println("Connection Failed with Response Code: "+response.statusCode());
-                 System.exit(1);
+                 System.out.println("Connection Failed with Response Code: " + response.statusCode());
+                 System.exit(1); // Unsuccessful termination
              }
          }catch(IOException | InterruptedException e){
              e.printStackTrace();
-             System.exit(1);
+             System.exit(1); // Unsuccessful termination
          }
      }
 }
