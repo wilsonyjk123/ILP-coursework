2b23e1a0ead17067afb3abe488a2bfdf46d178a8
Win102020NJPLUC\Administrator <s1941321@ed.ac.uk>
2021-10-03 23:18:29 +0100

    Upload New Files

--- ilp_cw1/src/main/java/uk/ac/ed/inf/App.java
+++ ilp_cw1/src/main/java/uk/ac/ed/inf/App.java
@@ -17,9 +17,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Menus menus = new Menus("localhost", "9898");
-        menus.getResponse();
-        String a = menus.getJsonText();
-        System.out.println(a);
     }
 }
